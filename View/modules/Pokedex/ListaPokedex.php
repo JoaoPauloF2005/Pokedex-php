e0543febf6708ef172a7287df41eb020ed1ff78a
Joao <hypergamerfrr@gmail.com>
2022-08-24 11:01:35 -0300

    Atualizado-Listar

--- View/modules/Pokedex/ListaPokedex.php
+++ View/modules/Pokedex/ListaPokedex.php
@@ -1,46 +1,52 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista Pessoas</title>
+    <title>Lista Nomes</title>
 </head>
 <body>
 
-    <table>
-        <tr>
-            <th></th>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>Hablidades</th>
-            <th>tipo</th>
-            <th>Peso</th>
-            <th>Altura</th>
-            <th>Imagem</th>
-
-        </tr>
+   
 
         <?php foreach($model->rows as $item): ?>
-        <tr>
-            <td>
-                <a href="/pokedex/delete?id=<?= $item->id ?>">X</a>
-            </td>
+            <div class="card">
 
-            <td><?= $item->id ?></td>
+            <div class="card">
+                <img class="card-img-top" src="<?= $item['imagem'] ?>">
+         
+         <ul class="list-group list-group-flush">
+                 <li class="list-group-item" style="color:#212121">Nome do Pokemon: <?= $item['nome'] ?></li>
+             </ul>
 
-            <td>
-                <a href="/pokedex/form?id=<?= $item->id ?>"><?= $item->nome ?></a>
-            </td>
+             <ul class="list-group list-group-flush">
+                 <li class="list-group-item" style="color:#212121">Hablidades: <?= $item['habilidades'] ?></li>
+             </ul>
 
-            <td><?= $item->hablidades ?></td>
-            <td><?= $item->tipo ?></td>
-            <td><?= $item->peso ?></td>
-            <td><?= $item->altura ?></td>
-            <td><?= $item->imagem ?></td>
-          
+                <ul class="list-group list-group-flush">
+                 <li class="list-group-item" style="color:#212121">Tipo: <?= $item['tipo'] ?></li>
 
-        </tr>
+                 <ul class="list-group list-group-flush">
+                 <li class="list-group-item" style="color:#212121">Peso: <?= $item['peso'] ?></li>
+             </ul>
+
+             <ul class="list-group list-group-flush">
+                 <li class="list-group-item" style="color:#212121">Altura: <?= $item['altura'] ?></li>
+             </ul>
+
+            
+
+                 <div class="card-body">
+
+                 <button type="button" class="btn btn-dark">
+                     <a href="/pokedex/form?id=<?= $item['id'] ?>">Editar Pokemon</a> 
+                 </button>
+
+                 <button type="button" class="btn btn-dark">
+                     <a href="/pokedex/delete?id=<?= $item['id'] ?>">Deletar</a> 
+                 </button>
+             </div>
         <?php endforeach ?>
 
         
