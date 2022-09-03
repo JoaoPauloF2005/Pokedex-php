c7b3334a82a8409a855d868fe27570856f143645
Joao <hypergamerfrr@gmail.com>
2022-09-03 00:26:10 -0300

    .

--- View/modules/Pagina_Inicial/home.php
+++ View/modules/Pagina_Inicial/home.php
@@ -5,15 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/../../css/estilo_home.css">
-    <link rel="shortcut icon" href="/../../img/simbolo_pokemon.jpg" type="image/x-icon">
-    
+    <link rel="shortcut icon" href="/../../img/simbolo_pokemon.png" type="image/x-icon">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+</style>
 
     <title>Pagina inicial</title>
 </head>
 <body>
 
     <div id="menu-h">
-        <a href="/home"> <img src="img/simbolo_pokemon.jpg"></a>
+        <a href="/home"> <img src="img/simbolo_pokemon.png"></a>
         <ul>
          
           
