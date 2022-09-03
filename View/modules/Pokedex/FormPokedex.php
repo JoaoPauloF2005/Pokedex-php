d49e91bb66001534cd674b390fd94e99781ca8e5
Joao <hypergamerfrr@gmail.com>
2022-09-02 22:56:33 -0300

    .

--- View/modules/Pokedex/FormPokedex.php
+++ View/modules/Pokedex/FormPokedex.php
@@ -4,17 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+     integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="/../../css/estilo_formPokedex.css">
+    <link rel="stylesheet" href="/../../css/estilo_home.css">
     <link rel="shortcut icon" href="/../../img/simbolo_pokemon.jpg" type="image/x-icon">
     
 
     <title>Cadastro de Pokémons</title>
    
-    <style>
-        
-    
-    </style>
 </head>
 <body>
 
